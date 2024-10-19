c1899f1112baa5ce183dfdcb47132db41f9426b1
EdwinGuerreroCortez <20221030@uthh.edu.mx>
2024-10-19 00:46:05 -0600

    add:node

--- db.js
+++ db.js
@@ -1,21 +1,24 @@
 const mysql = require('mysql2');
 
-// Configuración de la conexión a MySQL
-const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '',  // O tu contraseña de MySQL si tienes una
-  database: 'consultorio_dental',  // El nombre de la base de datos que creaste
-  port: 3306  // Puerto de MySQL (3306 es el puerto por defecto)
+// Configuración del pool de conexiones a MySQL (Hostinger)
+const pool = mysql.createPool({
+  host: '193.203.166.102',
+  user: 'u666156220_consultorio',
+  password: 'consultorioDental24$',
+  database: 'u666156220_consulDental',
+  port: 3306,
+  connectionLimit: 10,  // Número máximo de conexiones en el pool
 });
 
-// Conectar a la base de datos
-db.connect((err) => {
+// Verificar la conexión al crear el pool
+pool.getConnection((err, connection) => {
   if (err) {
     console.error('Error conectando a la base de datos:', err.message);
     return;
   }
   console.log('Conexión a MySQL exitosa');
+  connection.release();  // Liberar la conexión después de usarla
 });
 
-module.exports = db;  // Exporta la conexión para usarla en otros archivos
+module.exports = pool;  // Exportar el pool para usarlo en otros archivos
+
