981d68fe4eab1b70dfef4eda4873d9e04867be5b
EdwinGuerreroCortez <20221030@uthh.edu.mx>
2024-10-20 15:59:22 -0600

    add:recuperacionContraseña

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const cors = require('cors');
-const xss = require('xss-clean'); // Importa xss-clean
-require('dotenv').config();
+const xss = require('xss-clean');
 const cookieParser = require('cookie-parser');
+const csrf = require('csurf'); // Importar csurf
+require('dotenv').config();
 const authRoutes = require('./routes/authRoutes');
 
 const app = express();
@@ -11,19 +12,19 @@ const app = express();
 console.log('Entorno actual:', process.env.NODE_ENV);
 console.log('Puerto definido:', process.env.PORT);
 
-// Middleware
+// Middleware para parsear JSON y cookies
 app.use(express.json());
 app.use(cookieParser());
 
 // Middleware de xss-clean para evitar inyecciones XSS
-app.use(xss()); // Este middleware limpia las entradas de scripts maliciosos
+app.use(xss());
 
-// Configurar CORS
+// Configurar CORS para aceptar solicitudes de frontends específicos
 const corsOptions = {
   origin: ['https://consultoriodental.isoftuthh.com', 'http://localhost:3000'],
-  credentials: true,  // Esto permite enviar cookies con las solicitudes
+  credentials: true,  // Permitir envío de cookies
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'CSRF-Token'],  // Incluir CSRF token en allowedHeaders
 };
 
 app.use(cors(corsOptions));
@@ -31,16 +32,27 @@ app.use(cors(corsOptions));
 // Asegurar que el preflight de OPTIONS esté habilitado
 app.options('*', cors(corsOptions));
 
+// Configuración de CSRF usando cookies
+const csrfProtection = csrf({ cookie: true });
+app.use(csrfProtection);  // Aplicar la protección CSRF en toda la app
+
+// Ruta para obtener el token CSRF
+app.get('/api/get-csrf-token', (req, res) => {
+  res.cookie('XSRF-TOKEN', req.csrfToken(), { httpOnly: true, secure: true, sameSite: 'None' });
+  res.json({ csrfToken: req.csrfToken() });
+});
+
 // Usar las rutas de autenticación
 app.use('/api', authRoutes);
 
 // Middleware para manejar errores globales
 app.use((err, req, res, next) => {
+  if (err.code === 'EBADCSRFTOKEN') {  // Manejar errores de CSRF
+    return res.status(403).json({ message: 'Fallo en la validación del CSRF token' });
+  }
   console.error('Error:', err.stack);
   res.status(500).send('¡Algo salió mal en el servidor!');
 });
-
-console.log('Entorno actual:', process.env.NODE_ENV);
 
 // Iniciar servidor
 const PORT = process.env.PORT || 4000;
