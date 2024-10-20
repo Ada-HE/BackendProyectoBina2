3b457af8161e48464764aa6bc1067e2ce5a15d3b
EdwinGuerreroCortez <20221030@uthh.edu.mx>
2024-10-19 18:53:19 -0600

    add:cookie1.5

--- controllers/authController.js
+++ controllers/authController.js
@@ -318,7 +318,7 @@ const logout = (req, res) => {
     secure: true, // Igual que cuando la cookie fue creada
     sameSite: 'None',  // Igual que cuando la cookie fue creada
     path: '/',  // Asegúrate de que el path sea el mismo que cuando se creó la cookie
-    domain: '.isoftuthh.com', 
+    domain: 'consultoriodental.isoftuthh.com', 
     expires: new Date(0),  // Fecha de expiración pasada para eliminar la cookie
   });
 
